4c4ea97eecd9f72806f8470fed56228224632595
Alisson <alisson_942@outlook.com>
2019-12-09 20:59:09 -0300

    aula-05 envio de formulario

--- index.js
+++ index.js
@@ -9,13 +9,24 @@ nunjucks.configure('views', {
     watch: true,
 })
 
+app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'njk')
 
+const users = ["Alisson", "Aline", "Cristiane"]
+
 app.get('/', (req, res) => {
-    return res.render('list', {
-        name: 'fera'
-    })
+    return res.render('list', { users })
 })
 
+app.get('/new', (req, res) => {
+    return res.render('new')
+})
+
+
+app.post('/create', (req, res) => {
+    users.push(req.body.user)
+
+    return res.redirect('/')
+})
 
 app.listen(3000)
