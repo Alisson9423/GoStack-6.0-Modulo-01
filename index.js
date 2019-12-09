17a980b68848c95e8d7e1f0e4a1fdb1434ff623d
Alisson <alisson_942@outlook.com>
2019-12-09 20:09:23 -0300

    aula-02

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const http = require('http')
+const express = require("express")
 
-http.createServer((req, res) => {
-    console.log(req);
-    return res.end('Olá HTTP')
-}).listen(3000)
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send(`Bem-Vindo ${req.query.name}`)
+})
+
+app.get('/nome/:name', (req, res) => {
+    // return res.send(`Bem-Vindo, ${req.params.name}`)
+    return res.json({
+        message: `Bem-Vindo ${req.params.name}`
+    })
+})
+
+app.listen(3000)
