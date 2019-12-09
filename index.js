46716cae75af812d70b0e7d164c4fd3bdfb3bf8a
Alisson <alisson_942@outlook.com>
2019-12-09 20:24:14 -0300

    aula-03 middlewares

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const express = require("express")
 
 const app = express();
 
-app.get('/', (req, res) => {
+
+const logMiddleware = (req, res, next) => {
+    console.log(
+        `HOST:${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+    )
+
+    req.appName = "GoNode"
+    return next()
+}
+
+app.use(logMiddleware)
+
+app.get('/', logMiddleware, (req, res) => {
     return res.send(`Bem-Vindo ${req.query.name}`)
 })
 
 app.get('/nome/:name', (req, res) => {
     // return res.send(`Bem-Vindo, ${req.params.name}`)
     return res.json({
-        message: `Bem-Vindos ${req.params.name}`
+        message: `Bem-Vindos ${req.appName} ${req.params.name}`
     })
 })
+
+
+
 
 app.listen(3000)
