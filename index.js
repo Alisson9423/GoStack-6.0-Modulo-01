38966a59e4170cf82d40034ff61edbb18795b193
Alisson <alisson_942@outlook.com>
2019-12-09 20:35:25 -0300

    aula-04 nunjucks

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const express = require("express")
+const nunjucks = require('nunjucks')
+
 
 const app = express();
-
-
-const logMiddleware = (req, res, next) => {
-    console.log(
-        `HOST:${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-    )
-
-    req.appName = "GoNode"
-    return next()
-}
-
-app.use(logMiddleware)
-
-app.get('/', logMiddleware, (req, res) => {
-    return res.send(`Bem-Vindo ${req.query.name}`)
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app,
+    watch: true,
 })
 
-app.get('/nome/:name', (req, res) => {
-    // return res.send(`Bem-Vindo, ${req.params.name}`)
-    return res.json({
-        message: `Bem-Vindos ${req.appName} ${req.params.name}`
+app.set('view engine', 'njk')
+
+app.get('/', (req, res) => {
+    return res.render('list', {
+        name: 'fera'
     })
 })
-
-
 
 
 app.listen(3000)
